268a39d6bdfea24f53307aec2570dbf37fbe3fa4
Grant <grantsrockets@gmail.com>
2021-04-30 21:30:24 -0700

    template recyclerview template code added

--- app/src/main/java/com/example/ddapp/FirstFragment.java
+++ app/src/main/java/com/example/ddapp/FirstFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FirstFragment extends Fragment {
 
